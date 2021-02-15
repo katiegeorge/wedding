3cf1563fcddd668ceeb5853f5a61fe132dd1c5e4
Katie George <katiegeorge94@gmail.com>
2021-02-14 16:41:17 -0800

    purple

--- src/components/GettingHere.jsx
+++ src/components/GettingHere.jsx
@@ -4,7 +4,10 @@ import { Box, jsx, Heading } from "theme-ui";
 
 const Details = ({ className }) => {
   return (
-    <Box className={className} sx={{ minHeight: "100vh", bg: "green", p: 4 }}>
+    <Box
+      className={className}
+      sx={{ minHeight: "100vh", bg: "purple-light", p: 4 }}
+    >
       <Heading as="h1">Getting here</Heading>
       <Heading>Coming soon!</Heading>
     </Box>

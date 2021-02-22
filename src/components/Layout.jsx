05b3743dc4db7f2429a4e31f0ee2e48ad9ca11da
Katie George <katiegeorge94@gmail.com>
2021-02-22 11:14:02 -0800

    add site metadata

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,6 +5,14 @@ import Helmet from "react-helmet";
 import { Box, jsx } from "theme-ui";
 
 const Layout = ({ children, className }) => {
+  const base = {
+    title: `Katie and Meg's Wedding`,
+    product: `Katie and Meg's Wedding`,
+    description: `Details and information about Katie and Meg's Wedding in June 2022!`,
+    image: `https://katieandmeg.com/katie-meg-logo.png`,
+    author: `@katiegeorge`,
+  };
+
   return (
     <Box
       sx={{
@@ -62,10 +70,42 @@ const Layout = ({ children, className }) => {
 
         `}
       />
-      <Helmet
-        title="Katie and Meg's Wedding"
-        htmlAttributes={{ lang: "en" }}
-      ></Helmet>
+      <Helmet htmlAttributes={{ lang: "en" }}>
+        <title>Katie and Meg's Wedding</title>
+        <link rel="canonical" href={`https://katieandmeg.com`} />
+        <meta name="description" content={base.description} />
+        <meta name="image" content={base.image} />
+        <meta name="application-name" content={base.product} />
+        <meta name="apple-mobile-web-app-title" content={base.product} />
+        <link rel="icon" href={`https://katieandmeg.com/favicon.ico`} />
+        <link
+          rel="mask-icon"
+          href={`https://katieandmeg.com/favicon.ico`}
+          color="#000000"
+        />
+        <link
+          rel="apple-touch-icon"
+          href={`https://katieandmeg.com/favicon.ico`}
+        />
+
+        <meta property="og:url" content={`https://katieandmeg.com`} />
+        <meta property="og:site_name" content={base.product} />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={base.title} />
+        <meta property="og:description" content={base.description} />
+        <meta property="og:image" content={base.image} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="628" />
+        <meta property="og:image:alt" content={`${base.product} logomark`} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content={base.author} />
+        <meta name="twitter:title" content={base.title} />
+        <meta name="twitter:description" content={base.description} />
+        <meta name="twitter:image" content={base.image} />
+        <meta name="twitter:site" content={base.author} />
+        <meta name="twitter:creator" content={base.author} />
+      </Helmet>
       <main>{children}</main>
     </Box>
   );

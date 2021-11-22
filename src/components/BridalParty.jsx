87083c1378f89afa0999b5220fdaf940b453d943
Katie George <katiegeorge94@gmail.com>
2021-11-22 17:56:31 -0500

    adds bridal party and initial logic

--- src/components/BridalParty.jsx
+++ src/components/BridalParty.jsx
@@ -1,40 +1,129 @@
 /** @jsx jsx */
 
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { jsx, Flex, Heading, Image } from "theme-ui";
-import Avatar from "../images/avatar.png";
+import katie from "../images/wedding-party/katie.png";
+import jack from "../images/wedding-party/jack.png";
+import brianna from "../images/wedding-party/brianna.png";
+import leah from "../images/wedding-party/leah.png";
+import sandra from "../images/wedding-party/sandra.png";
+import ellen from "../images/wedding-party/ellen.png";
+import micelli from "../images/wedding-party/micelli.png";
+import abigail from "../images/wedding-party/abigail.png";
+import sophia from "../images/wedding-party/sophia.png";
+import marija from "../images/wedding-party/marija.png";
+import anna from "../images/wedding-party/anna.png";
+import sasa from "../images/wedding-party/sasa.png";
+import kerry from "../images/wedding-party/kerry.png";
+
 import Section from "./Section";
 
 const BridalParty = ({ className }) => {
   const megPeople = [
     {
-      name: "TBD",
-      title: "Maid of Honor",
+      name: "Katie Delaney",
+      title: "Best Maid",
       description: "",
-      avatar: Avatar,
+      avatar: katie,
     },
     {
-      name: "TBD",
-      title: "Bridesmaids",
+      name: "Jack Delaney",
+      title: "Best Man",
       description: "",
-      avatar: Avatar,
+      avatar: jack,
+    },
+    {
+      name: "Sandra Bamford",
+      title: "Bridesmaid",
+      description: "",
+      avatar: sandra,
+    },
+    {
+      name: "Leah Ellert",
+      title: "Bridesmaid",
+      description: "",
+      avatar: leah,
+    },
+    {
+      name: "Brianna Culp",
+      title: "Bridesmaid",
+      description: "",
+      avatar: brianna,
+    },
+    {
+      name: "Ellen McGrath",
+      title: "Bridesmaid",
+      description: "",
+      avatar: ellen,
+    },
+    {
+      name: "Lauren Micelli",
+      title: "Bridesmaid",
+      description: "",
+      avatar: micelli,
     },
   ];
 
   const katiePeople = [
     {
-      name: "TBD",
+      name: "Abigail George",
       title: "Maid of Honor",
       description: "",
-      avatar: Avatar,
+      avatar: abigail,
     },
     {
-      name: "TBD",
-      title: "Bridesmaids",
+      name: "Sophia Falmagne",
+      title: "Matron of Honor",
       description: "",
-      avatar: Avatar,
+      avatar: sophia,
+    },
+    {
+      name: "Marija Nadjsombati",
+      title: "Bridesmaid",
+      description:
+        "Katie’s friend from high school, AP study buddy, gossip friend" +
+        " and who convinced Katie to move to Seattle.",
+      mostLikely: "have a mini existential crisis about how we’re all getting so old",
+      avatar: marija,
+    },
+    {
+      name: "Anna Lovelace",
+      title: "Bridesmaid",
+      description: "",
+      avatar: anna,
+    },
+    {
+      name: "Kerry Driscoll",
+      title: "Bridesmaid",
+      description: "",
+      avatar: kerry,
+    },
+    {
+      name: "Sasa Schwartz",
+      title: "Bridesmaid",
+      description: "",
+      avatar: sasa,
     },
   ];
+
+  const [showDescription, setShowDescription] = useState([]);
+
+
+  const handleClick = (id) => {
+    console.log(showDescription);
+
+    if (showDescription.includes(id)) {
+      if (showDescription.length === 1) {
+        setShowDescription([]);
+        return;
+      }
+      setShowDescription(showDescription.filter(item => item !== id));
+
+      return;
+    }
+
+    setShowDescription([...showDescription, id]);
+  }
 
   return (
     <Section heading="Bridal Party" id="bridal-party" className={className}>
@@ -46,57 +135,82 @@ const BridalParty = ({ className }) => {
           <div sx={{ flex: "1", mr: [0, 5] }}>
             <p sx={{ mt: 4 }}>
               <Heading sx={{ mb: 1 }}>Meg</Heading>
-              {/* {megPeople.map((person) => (
-                <div>
-                  <Flex sx={{ mt: 5, alignItems: "center" }}>
+              {megPeople.map((person) => (
+                <div sx={{
+                  p: 2,
+                  background: "#1F3E6D",
+                  borderRadius: '8px',
+                  boxShadow: '3px 3px 2px #071934',
+                  mt: 3,
+                  transition: '0.2s',
+                  ":hover": {
+                    backgroundColor: "#3F5E8C",
+                    cursor: "pointer",
+                    transform: "translateY(2px)"
+                  },
+                }}>
+                  <Flex sx={{ alignItems: "center" }}>
                     <Image
                       src={person.avatar}
                       sx={{
-                        bg: "blue-dark",
                         p: 3,
                         mr: 3,
-                        borderRadius: "50%",
-                        height: "100px",
-                        width: "100px",
+                        height: "150px",
                       }}
                     />
                     <div>
-                      <Heading>{person.name}</Heading>
+                      <Heading sx={{ color: "green-light" }}>{person.name}</Heading>
                       <p>{person.title}</p>
                     </div>
                   </Flex>
                   <p>{person.description}</p>
                 </div>
-              ))} */}
-              Coming soon!
+              ))}
             </p>
           </div>
           <div sx={{ flex: "1" }}>
             <p sx={{ mt: 4 }}>
               <Heading sx={{ mb: 1 }}>Katie</Heading>
-              {/* {katiePeople.map((person) => (
-                <div>
-                  <Flex sx={{ mt: 5, alignItems: "center" }}>
+              {katiePeople.map((person, index) => (
+                <div
+                  id={`party-member-${index}`}
+                  sx={{
+                    p: 3,
+                    background: "#1F3E6D",
+                    borderRadius: '8px',
+                    boxShadow: '3px 3px 2px #071934',
+                    mt: 3,
+                    transition: '0.2s',
+                    ":hover": {
+                      backgroundColor: "#3F5E8C",
+                      cursor: "pointer",
+                      transform: "translateY(2px)"
+                    },
+                  }}
+                  onClick={() => handleClick(index)}
+                >
+                  <Flex sx={{ alignItems: "center" }}>
                     <Image
                       src={person.avatar}
                       sx={{
-                        bg: "blue-dark",
-                        p: 3,
+                        p: 2,
                         mr: 3,
-                        borderRadius: "50%",
-                        height: "100px",
-                        width: "100px",
+                        height: showDescription.includes(index) ? "100px" : "150px",
                       }}
                     />
                     <div>
-                      <Heading>{person.name}</Heading>
+                      <Heading sx={{ color: "blue-light", fontSize: showDescription ? 4 : 5 }}>{person.name}</Heading>
                       <p>{person.title}</p>
                     </div>
                   </Flex>
-                  <p>{person.description}</p>
+                  {showDescription.includes(index) &&
+                    <Fragment>
+                      <p>{person.description}</p>
+                      <p>Most likely to... {person.mostLikely}</p>
+                    </Fragment>
+                  }
                 </div>
-              ))} */}
-              Coming soon!
+              ))}
             </p>
           </div>
         </Flex>
